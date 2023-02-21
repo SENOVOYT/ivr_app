fea529d101903630699a84c81dea3c2aec0e30cc
Driane Diojan Perez <drianeperez12@gmail.com>
2023-02-21 09:24:28 -0600

    fixed position link

--- app/Http/Livewire/AddCategoryLink.php
+++ app/Http/Livewire/AddCategoryLink.php
@@ -3,8 +3,10 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use App\Models\UserCategory;
 use App\Models\UserLinks;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use LivewireUI\Modal\ModalComponent;
 
@@ -43,7 +45,7 @@ class AddCategoryLink extends ModalComponent
         }
     }
     public function save(){
-
+        $this->user_id=Auth::user()->getId();
 
         if(count($this->addlink_name)>1){
             
@@ -56,12 +58,9 @@ class AddCategoryLink extends ModalComponent
 
         
         $validator = Validator::make([
-            'custom_addlink_name'=>$this->custom_addlink_name,
             'addlink_name' => $this->addlink_name[0],
         ],[
-            'custom_addlink_name' => ['required', 'string', 'min:3','max:22'],
-            'addlink_name' => ['required', 'integer',],
-            
+            'addlink_name' => ['required', 'integer']  
         ]);
         
         if ($validator->fails()) {
@@ -71,10 +70,33 @@ class AddCategoryLink extends ModalComponent
             foreach($error->all() as $error){
                 $collection.=$error.' ';
             }
-            session()->flash('message_customlink_validator_error', $collection);
-            $this->emit('message_customlink_validator_error');
+            session()->flash('message_customlinkadd_validator_error', $collection);
+            $this->emit('message_customlinkadd_validator_error');
             return 0;
         }
+        $id=UserLinks::where('id',$this->addlink_name[0])->get();
+        if(!(count($id)==1)){
+            session()->flash('message_customlinkadd_validator_error', 'Invalid link name');
+            $this->emit('message_customlinkadd_validator_error');
+            return 0;
+        }
+        $last_id=DB::table('user_links')
+        ->where('category',$this->category)
+        ->where('user',$this->user_id)
+        ->latest('position')
+        ->first();
+        
+        UserLinks::where('id',$this->addlink_name[0])
+        ->update([
+            'category' => $this->category,
+            'position' => $last_id? $last_id->position + 1 : 1
+
+        ]);
+        $this->emit('addedlink'.$this->category);
+        $this->closeModal();
+        
+
+
 
     }
 }
--- resources/views/livewire/category-layout.blade.php
+++ resources/views/livewire/category-layout.blade.php
@@ -9,9 +9,19 @@
                                     <div wire:sortable.handle class=" mr-auto flex">
                                         <svg  xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1  dark:fill-white fill-black rounded-md text-gray-500  cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600 "  viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 9H5c-.55 0-1 .45-1 1s.45 1 1 1h14c.55 0 1-.45 1-1s-.45-1-1-1zM5 15h14c.55 0 1-.45 1-1s-.45-1-1-1H5c-.55 0-1 .45-1 1s.45 1 1 1z"/></svg>
                                         {{ $categories->category_name }}
+                                        {{-- created --}}
+        
                                     </div>
                                     
                                      <div class="   items-end gap-1  flex">
+                                        <div x-data="{ show: false, timeout: null }"
+        x-init="@this.on('addedlink{{ $categories->id }}', () => { clearTimeout(timeout); show = true; timeout = setTimeout(() => { show = false }, 2000); })"
+        x-show.transition.opacity.out.duration.1500ms="show"
+        class="flex text-sm ml-1 text-green-800 rounded-lg  dark:text-green-400" role="alert">
+        <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                
+        <span class="font-medium">Added Link!</span>
+    </div>
                                         <a wire:click="$emit('openModal','see-category-link', {{ json_encode(["category" => $categories->id]) }})'" class=" text-gray-500 rounded cursor-pointer dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600" ">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-5 fill-green-400 w-5 rounded-md " fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
                                         </a>
